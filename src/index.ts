0094d968d6a32443102826166a6213542a21bf76
Ahoia <42796524+ahoiadigital@users.noreply.github.com>
2023-06-09 07:52:31 +1000

    Remove gret user

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
-import { greetUser } from '$utils/greet';
-
 window.Webflow ||= [];
 window.Webflow.push(() => {
   const name = 'Louis Smith';
-  greetUser(name);
   document.body.style.background = 'blue';
 });
